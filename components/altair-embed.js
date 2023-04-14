1eb06754b058816f2b6edef34354c0a58a54cce7
liangth <liangth@umich.edu>
2023-04-14 14:14:12 -0400

    change width

--- components/altair-embed.js
+++ components/altair-embed.js
@@ -9,13 +9,6 @@ class AltairEmbed extends React.Component {
         width={this.props.width} 
         height={this.props.height}
         framemargin="0"
-        style={{
-            border: "0",
-            margin: "0",
-            padding: "0",
-            position: 'relative',
-            left: '-300px',
-        }}
       />
     );
   }
